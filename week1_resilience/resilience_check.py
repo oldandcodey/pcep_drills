39c2e207cc8f1df17fb3ae136b45a50c8a8cf7fb
Bob Pelletier <pnwbob70@gmail.com>
2026-09-12 13:18:09 -0500

    Initial Commit

---
+++ week1_resilience/resilience_check.py
@@ -0,0 +1,7 @@
+print("Navarre Resilience Check Entry", end="\n")
+name = input("operator name: ")
+print("Logged in as",name, sep=": ")
+date = input("todays date: ")
+
+print("NAVARRE RESILIENCE CHECK", end="\n")
+print("Prepared by", name, "on", date)
