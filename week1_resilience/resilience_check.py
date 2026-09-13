c8e58a97775b9217eb8f5c71955093a1717293ea
Bob Pelletier <pnwbob70@gmail.com>
2026-09-13 06:29:33 -0500

    Add Day 2 banner, operator and casting

--- week1_resilience/resilience_check.py
+++ week1_resilience/resilience_check.py
@@ -2,6 +2,10 @@ print("Navarre Resilience Check Entry", end="\n")
 name = input("operator name: ")
 print("Logged in as",name, sep=": ")
 date = input("todays date: ")
+gallons_on_hand = input("how many gallons of gas do you have? ")
+gallons_required = 5
+gallon_shortfall = gallons_required - int(gallons_on_hand)
 
 print("NAVARRE RESILIENCE CHECK", end="\n")
 print("Prepared by", name, "on", date)
+print("gallons of gas on hand ", gallons_on_hand, "Gallons Required: ", gallons_required, "Shortfall: ", gallon_shortfall)
